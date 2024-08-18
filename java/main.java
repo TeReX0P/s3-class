4efd5882ef9d81c0ed4af5034fc51cc1a768b210
nihalzown <nihalnihu710@Gmail.com>
2024-08-18 09:29:24 +0530

    change in student marks

--- java/main.java
+++ java/main.java
@@ -18,16 +18,7 @@ public class main{
             s[i] = new student(name, marks);
         }
         System.out.println("Name and marks of 5 students: ");
-        for(int i=0; i<5; i++){
-            for(int j=i+1; j<5; j++){
-                if(s[i].marks > s[j].marks){
-                    student temp = s[i];
-                    s[i] = s[j];
-                    s[j] = temp;
-                }
-            }
-            System.out.println(s[i].name + " " + s[i].marks);
-        }
+        System.out.println(s[i].name + " " + s[i].marks);
         System.out.println("average marks: ");
         int sum = 0;
         for(int i=0; i<5; i++){
