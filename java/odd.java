5cc9ae183704d5808cc648569b47c551c000d1c7
MUHAMMED NIHAL <145740029+nihalzown@users.noreply.github.com>
2024-10-07 07:39:55 +0530

    updated lab oct 7

---
+++ java/odd.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class OddNumberException extends Exception {
+    public OddNumberException(String message) {
+        super(message);
+    }
+}
+
+public class odd{
+    public static void checkOddNumber(int number) throws OddNumberException {
+        if (number % 2 != 0) {
+            throw new OddNumberException("Exception: The number " + number + " is odd.");
+        } else {
+            System.out.println("The number " + number + " is even.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to check: ");
+        int number = scanner.nextInt();
+
+        try {
+            checkOddNumber(number);
+        } catch (OddNumberException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
